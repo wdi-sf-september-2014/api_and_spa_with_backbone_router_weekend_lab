8fbfc76686801d0269c591101f96f1044fe1bfe0
Alex Notov <anotov@gmail.com>
2014-12-07 23:01:49 -0800

    users, posts, routes

--- medium_clone_api/config/routes.rb
+++ medium_clone_api/config/routes.rb
@@ -1,2 +1,12 @@
 MediumCloneApi::Application.routes.draw do
+  constraints subdomain: 'api' do
+    resources :users, only: [:create] do
+      resources :posts, except: [:new, :edit, :destroy]
+    end
+
+    get 'posts/index'
+    post 'users/login'
+  end
+
+  root 'users#login'
 end
