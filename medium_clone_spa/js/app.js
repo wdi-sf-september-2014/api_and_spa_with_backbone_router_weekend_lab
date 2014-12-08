9e36822ea2d2067c016d617a1920be8f501d6210
Alex Notov <anotov@gmail.com>
2014-12-08 00:08:30 -0800

    Router now working for index. Also forms

--- medium_clone_spa/js/app.js
+++ medium_clone_spa/js/app.js
@@ -1,8 +1,26 @@
-var source   = $("#posts_template").html();
-var template = Handlebars.compile(source);
+$(function() {
 
-$.ajax( "http://api.medium-clone-api.dev/posts.json", {
-  complete: function( data ) {
-    $('#posts').html(template({posts: data.responseJSON}));
-  }
+  var Router = Backbone.Router.extend({
+
+    routes: {
+      "":                     "index", // root
+      "index":                "index"  // index
+    },
+
+    index: function() {
+      var source   = $("#posts_template").html();
+      var template = Handlebars.compile(source);
+
+      $.ajax( "http://api.medium-clone-api.dev/posts.json", {
+        complete: function( data ) {
+          $('#posts').html(template({posts: data.responseJSON}));
+        }
+      });
+    }
+
+  });
+
+  new Router();
+  Backbone.history.start();
+
 });
