3a340ebda253c87238f263ba88e62f2792a39662
Alex Notov <anotov@gmail.com>
2014-12-08 00:41:34 -0800

    Signing up users and storing auth token in sessionStorage

--- medium_clone_api/app/controllers/users_controller.rb
+++ medium_clone_api/app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
   def create
+    if @user = User.create(user_params)
+      render "users/show", status: :created
+    end
   end
 
   def login
+  end
+
+  private
+  def user_params
+    params.require(:user).permit([:first, :last, :email, :password])
   end
 end
--- medium_clone_spa/js/app.js
+++ medium_clone_spa/js/app.js
@@ -5,6 +5,8 @@ $(function() {
     return Handlebars.compile(source);
   };
 
+  var root_uri =  "http://api.medium-clone-api.dev"
+
   var Router = Backbone.Router.extend({
 
     routes: {
@@ -15,7 +17,7 @@ $(function() {
     },
 
     index: function() {
-      $.ajax( "http://api.medium-clone-api.dev/posts.json", {
+      $.ajax(root_uri + "/posts.json", {
         complete: function( data ) {
           $('#container').html(compile_template("#posts_t")({posts: data.responseJSON}));
         }
@@ -24,10 +26,43 @@ $(function() {
 
     signup: function() {
       $('#container').html(compile_template("#signup_t")());
+      $('#signup').submit(function(event){
+        event.preventDefault();
+        
+        $.ajax(root_uri + "/users.json", {
+          method: "POST",
+          data: {
+            user: {
+              first: $('#signup input[name=first]').val(),
+              last: $('#signup input[name=last]').val(),
+              email: $('#signup input[name=email]').val(),
+              password: $('#signup input[name=password]').val()
+            }
+          },
+          complete: function ( data ) {
+            sessionStorage.setItem("auth_token", data.responseJSON.auth_token);
+            sessionStorage.setItem("user_id", data.responseJSON.id);
+          }
+        });
+      });
     },
 
     login: function() {
       $('#container').html(compile_template("#login_t")());
+      $('#login').submit(function(event){
+        event.preventDefault();
+
+        $.ajax(root_uri + "/users/login.json", {
+          method: "POST",
+          data: {
+            email: $('#login input[name=email]').val(),
+            password: $('#login input[name=password]').val()
+          },
+          complete: function ( data ) {
+            console.log(data);  
+          }
+        });
+      });
     }
 
   });
