e6a7e05474847ed91a715abf339a038139e3b637
Alex Notov <anotov@gmail.com>
2014-12-07 23:09:18 -0800

    Generating auth token for newly created users

--- medium_clone_api/app/models/user.rb
+++ medium_clone_api/app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  has_many :posts
+
+  before_create :set_auth_token
+
+  def set_auth_token
+    self.auth_token = generate_random_token
+  end
+
+  private
+  def generate_random_token
+    loop do
+      random_token = SecureRandom.hex
+      break random_token unless self.class.exists?(auth_token: random_token)
+    end
+  end
 end
--- medium_clone_api/db/schema.rb
+++ medium_clone_api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208070053) do
+ActiveRecord::Schema.define(version: 20141208070357) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141208070053) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "auth_token"
   end
 
 end
